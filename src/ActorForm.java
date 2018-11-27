c16b48bbdeea21e5474c7e8c1d216e98d2211a6d
HyleKenson <kyle495@gmail.com>
2018-11-27 14:04:34 -0600

    Completed

--- src/ActorForm.java
+++ src/ActorForm.java
@@ -11,7 +11,7 @@ class ActorForm extends JPanel implements ActionListener {
 	
     private JButton submitButton;
 
-    JTextArea actorIDArea = new JTextArea(1, 50);
+    JTextField actorIDArea = new JTextField();
     JTextArea actorFnameArea = new JTextArea(1, 50);
     JTextArea actorLnameArea = new JTextArea(1, 50);
     JTextArea actorGenderArea = new JTextArea(1, 1);
@@ -29,14 +29,16 @@ class ActorForm extends JPanel implements ActionListener {
         //set component bounds 
         actorIDArea.setBorder(BorderFactory.createTitledBorder("Actor ID"));
         actorIDArea.setBounds (0, 0, 400, 50);
+        actorIDArea.setEditable(true);
         actorFnameArea.setBorder(BorderFactory.createTitledBorder("Actor First Name"));
         actorFnameArea.setBounds (0, 200, 400, 50);
+        actorFnameArea.setEditable(false);
         actorLnameArea.setBorder(BorderFactory.createTitledBorder("Actor Last Name"));
         actorLnameArea.setBounds (0, 250, 400, 50);
+        actorLnameArea.setEditable(false);
         actorGenderArea.setBorder(BorderFactory.createTitledBorder("Actor Gender"));
         actorGenderArea.setBounds (0, 300, 400, 50);
-
-
+        actorGenderArea.setEditable(false);
         submitButton.setBounds (0, 100, 100, 50);
 
 
@@ -52,40 +54,24 @@ class ActorForm extends JPanel implements ActionListener {
         submitButton.addActionListener(this);
     }
     // Sets text area using first row from actor
-    public void getRow(String row) {
+    public void getRow(int actorID) {
     	try {
     		Class.forName("com.mysql.cj.jdbc.Driver");
     		Connection con = DriverManager.getConnection("jdbc:mysql://localhost/ics311project?user=root&password=ics311");
     		System.out.println("Connection Object Created : " + con);
     		Statement st = con.createStatement();
             ResultSet rs = st.executeQuery("select * from actor");
-            int totalRows = 0;
-            while(rs.next()){
-                totalRows ++;
+            rs.absolute(actorID);
+            if(rs.next()) {
+            	rs.absolute(actorID);
+                actorFnameArea.setText(rs.getString(2));
+                actorLnameArea.setText(rs.getString(3));
+                actorGenderArea.setText(rs.getString(4));
+            } else {
+                actorFnameArea.setText("N/A");
+                actorLnameArea.setText("N/A");
+                actorGenderArea.setText("N/A");
             }
-            if(row == "First"){
-                rowCount = 1;
-                rs.absolute(rowCount);
-            } else if(row == "Previous"){
-                if(rowCount > 1){
-                    rowCount --;
-                }
-                rs.absolute(rowCount);
-            } else if(row == "Next"){
-                if(rowCount < totalRows){
-                    rowCount ++;
-                }
-                rs.absolute(rowCount);
-            } else if(row == "Last"){
-                rowCount = totalRows;
-                rs.last();
-            }
-
-            actorIDArea.setText(rs.getString(1));
-            actorFnameArea.setText(rs.getString(2));
-            actorLnameArea.setText(rs.getString(3));
-            actorGenderArea.setText(rs.getString(4));
-
     		st.close();
             rs.close();
         } catch (Exception ex) { ex.printStackTrace();}
@@ -95,7 +81,8 @@ class ActorForm extends JPanel implements ActionListener {
 	public void actionPerformed(ActionEvent event) {
 		Object srcObj = event.getSource();
 		if (srcObj == submitButton) {
-            getRow("First");
+			int actorID = Integer.parseInt(actorIDArea.getText());
+            getRow(actorID);
 		}
 	}
 }
